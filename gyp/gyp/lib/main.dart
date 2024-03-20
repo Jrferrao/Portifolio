fde4be8b456eaf1c7b701934b7ba849e54133cbe
Jrferrao <jrferrao@gmail.com>
2024-03-20 13:41:45 -0300

    cores e paginas

--- gyp/gyp/lib/main.dart
+++ gyp/gyp/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gyp/view/login_page.dart';
+
+
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +15,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: LoginPage(),
         ),
       ),
     );
---
+++ gyp/gyp/lib/view/login_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:gyp/view/signup_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _inputField(context),
+              _forgotPassword(context),
+              _signup(context),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        
+        
+        Text(
+          "G.Y.P",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Gym in Your Pocket",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text("Seu treino acessível a qualquer momento",
+          style: TextStyle(fontSize: 14, ),
+        ),
+        
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Usuário",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none
+              ),
+              fillColor: Colors.cyan.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Senha",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Colors.cyan.withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: ( ) { 
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.cyan,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 20),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword(context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text("Esqueceu sua senha?",
+        style: TextStyle(color: Colors.cyan),
+      ),
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Não possui uma conta?"),
+        TextButton(
+            onPressed: () {
+            },
+            child: const Text("Se Inscreva", style: TextStyle(color: Colors.cyan),)
+        )
+      ],
+    );
+  }
+}
